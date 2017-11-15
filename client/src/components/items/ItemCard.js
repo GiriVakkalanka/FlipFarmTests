63d98a55be334cbc034acc7d80c3dccf83869d57
Giri Vakkalanka <gvakka@gmail.com>
2017-11-14 23:54:05 -0500

    config check

--- client/src/components/items/ItemCard.js
+++ client/src/components/items/ItemCard.js
@@ -6,14 +6,19 @@ import * as actions from '../../actions';
 
 class ItemCard extends Component {
   render() {
+    //config check
     //console.log(this.props);
     const buttonLabel = this.props.buttonLabel;
     return (
       <Row>
         <Col xs={12}>
           <Thumbnail src={this.props.src} alt="item">
-            <h3>{this.props.name}</h3>
-            <p>{this.props.description}</p>
+            <h3>
+              {this.props.name}
+            </h3>
+            <p>
+              {this.props.description}
+            </p>
             <p>
               <Link to={`/offer/${this.props._id}`}>
                 <Button
