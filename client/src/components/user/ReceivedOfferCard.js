dc1a03d2766983a9f0102c0c097dcd029f675a3b
Giri <gvakka@gmail.com>
2018-01-19 09:41:05 -0500

    added address input modal on client

--- client/src/components/user/ReceivedOfferCard.js
+++ client/src/components/user/ReceivedOfferCard.js
@@ -6,21 +6,25 @@ import { Panel, Button } from 'react-bootstrap';
 class ReceivedOfferCard extends Component {
   render() {
     return (
-      <Panel>
-        <h3>{this.props.name}</h3>
-        <p>{this.props._itemOffered}</p>
-        <p>{this.props._offerFrom}</p>
-        <p>
-          <Button
-            onClick={() => {
-              this.props.handleAccept();
-            }}
-            bsStyle="primary"
-          >
-            Accept Offer
-          </Button>
-        </p>
-      </Panel>
+      <div>
+        <Panel>
+          <h3>{this.props.name}</h3>
+          <p>{this.props._itemOffered}</p>
+          <p>{this.props._offerFrom}</p>
+          <p>
+            <Button
+              onClick={() => {
+                this.props.handleAccept();
+                //this.handleShow();
+                //console.log(this.state);
+              }}
+              bsStyle="primary"
+            >
+              Accept Offer
+            </Button>
+          </p>
+        </Panel>
+      </div>
     );
   }
 }
--- client/src/components/user/ReceivedOfferList.js
+++ client/src/components/user/ReceivedOfferList.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Grid, Col } from 'react-bootstrap';
+import { Grid, Col, Modal, Button } from 'react-bootstrap';
+import AddressWidget from './AddressWidget';
 import ReceivedOfferCard from './ReceivedOfferCard';
 import * as actions from '../../actions';
 
 class ReceivedOfferList extends Component {
+  constructor(...args) {
+    super(...args);
+
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+
+    this.state = { showModal: false };
+  }
+
+  handleClose() {
+    this.setState({ showModal: false });
+  }
+
+  handleShow() {
+    this.setState({ showModal: true });
+    //console.log(this.state);
+  }
+
   renderItems() {
     //console.log(this.props.auth);
     //const itemWanted = this.props.itemWanted;
-    console.log(this.props.offersReceived);
+    //console.log(this.props.offersReceived);
     return this.props.offersReceived.map(offer => {
       const keyNum = Math.random();
       const offerObj = {
@@ -22,17 +41,42 @@ class ReceivedOfferList extends Component {
             name={offer.itemWanted.name}
             _itemOffered={offer.itemOffered.name}
             _offerFrom={offer.itemOffered._id}
-            handleAccept={() => this.props.acceptOffer(offerObj)}
+            handleAccept={() => {
+              this.props.acceptOffer(offerObj);
+              this.handleShow();
+              //console.log(this.props.auth);
+            }}
           />
         </Col>
       );
     });
   }
+
   render() {
     return (
-      <Grid>
-        <div>{this.renderItems()}</div>
-      </Grid>
+      <div>
+        <Grid>
+          <div>{this.renderItems()}</div>
+        </Grid>
+
+        <Modal
+          {...this.props}
+          show={this.state.showModal}
+          onHide={this.handleClose}
+          bsSize="large"
+          aria-labelledby="contained-modal-title-lg"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <AddressWidget />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.handleClose}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
     );
   }
 }
